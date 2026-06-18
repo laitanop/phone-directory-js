de92778a1ed7159d71cac569ce177aeab072a3a8
laitanop <laitanop@yahoo.com>
2026-06-18 15:36:30 -0600

    add contats summary User interface

---
+++ script.js
@@ -0,0 +1,9 @@
+let name = document.getElementById("name");
+let mobile = document.getElementById("mobile");
+let email = document.getElementById("email");
+
+function addVendor() {
+  console.log(name.value);
+  console.log(mobile.value);
+  console.log(email.value);
+}
